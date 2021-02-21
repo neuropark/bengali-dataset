5f791a12d1782c11fecc719de0e163e3a87e1a87
Tanmoy Sarkar <tanmoyf2@gmail.com>
2021-02-21 21:06:49 +0530

    added email authentication

--- config/post.py
+++ config/post.py
@@ -1,3 +1,4 @@
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 scope = [
@@ -7,7 +8,14 @@ scope = [
 creds = ServiceAccountCredentials.from_json_keyfile_name("config/creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open('Bengali Dataset (Responses)').sheet1
+with open('config/mail.json') as f:
+    data = dict(json.load(f))
 
-with open('entries.txt', 'r', encoding='utf8') as f:
-    for entry in f.readlines():
-        sheet.insert_row(['', entry.replace('\n', '')],3)
+if data["email"] == "ENTER YOUR EMAIL INSIDE THE QUOTE" or "" or "":
+    print("please enter a valid email adderss\nGo to config folder. Open the mail.json file and \nenter your email inside the quote containing 'ENTER YOUR EMAIL INSIDE THE QUOTE'")
+
+else:
+    with open('entries.txt', 'r', encoding='utf8') as f:
+        for entry in f.readlines():
+            sheet.insert_row(['', entry.replace('\n', ''), data["email"]],3)
+            print('DONE!')
--- main.bat
+++ main.bat
@@ -1,1 +1,2 @@
 python config/post.py
+
